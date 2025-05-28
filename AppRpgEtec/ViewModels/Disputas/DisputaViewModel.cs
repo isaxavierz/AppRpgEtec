ce373f60312dc2ee4e2c4de3260ba69320082986
Isabelly Xavier <isabellyxavier1323@hotmail.com>
2025-05-27 21:47:52 -0300

    Add Método Pesquisa de Personagem

---
+++ AppRpgEtec/ViewModels/Disputas/DisputaViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Personagens;
+using AppRpgEtec.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Disputas
+{
+    public class DisputaViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> PersonagensEncontrados {  get; set; }
+        public Personagem Atacante { get; set; }
+        public Personagem Oponente { get; set; }
+        public string DescricaoPersonagemAtacante
+        {
+            get => Atacante.Nome;
+        }
+        public string DescricaoPersonagemOponente
+        {
+            get => Oponente.Nome;
+        }
+        private Personagem personagemSelecionado;
+        public Personagem PersonagemSelecionado
+        {
+            set
+            {
+                if(value != null)
+                {
+                    personagemSelecionado = value;
+                    SelecionarPersonagem(personagemSelecionado);
+                    OnPropertyChanged();
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+
+
+
+        public DisputaViewModel() {
+            string token = Preferences.Get("UsuarioToken", String.Empty);
+
+            pService = new PersonagemService(token);
+
+            Atacante = new Personagem();
+            Oponente = new Personagem();
+
+            PersonagensEncontrados = new ObservableCollection<Personagem>();
+
+            PesquisarPersonagensCommand = new Command<string>(async (string pesquisa) => { await PesquisarPersonagens(pesquisa); });
+        }
+        
+        public ICommand PesquisarPersonagensCommand { get; set; }
+
+        public async Task PesquisarPersonagens(string textoPesquisaPersonagem)
+        {
+            try
+            {
+                PersonagensEncontrados = await pService.GetByNomeAproximadoAsync(textoPesquisaPersonagem);
+                OnPropertyChanged(nameof(PersonagensEncontrados));  
+            }
+            catch (Exception ex) {
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public async void  SelecionarPersonagem(Personagem p)
+        {
+            try
+            {
+                string tipoCombatente = await Application.Current.MainPage
+                    .DisplayActionSheet("Atacante ou Oponente?", "Cancelar", "", "Atacante", "Oponente");
+
+                if (tipoCombatente == "Atacante")
+                {
+                    Atacante = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemAtacante));
+                }
+                else if (tipoCombatente == "Oponente")
+                {
+                    Oponente = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemOponente));
+                }
+            }
+            catch (Exception ex) {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+
+    }
+}
